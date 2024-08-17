359d555567df8ef143d3b95cd29ea2f2a0700529
Hayley Ryan <hayley.ryan@mettle.co.uk>
2024-08-17 21:11:36 +0100

    update config

--- docs/src/components/Home.js
+++ docs/src/components/Home.js
@@ -4,11 +4,11 @@ import './Home.css';
 function Home() {
     return (
         <section className="content">
-            <h2 className="fade-in">Welcome to Group 4's data analysis project docs home!</h2>
+            <h2 className="fade-in">Welcome to Group 4's data analysis project docs home! </h2>
             <p className="fade-in delay-1">This website provides documentation for our project and explains the process behind our data analysis.</p>
             <p className="fade-in delay-2">Explore the sections above to learn more about the project and the steps the fantastic five took.</p>
         </section>
-    );
+    )
 }
 
 export default Home;
