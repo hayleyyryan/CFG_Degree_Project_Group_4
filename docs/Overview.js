672d944f5ec72192e1c3cbc2da744dc5818ac35a
Hayley Ryan <hayley.ryan@mettle.co.uk>
2024-08-17 18:53:38 +0100

    overhaul app with react

---
+++ docs/Overview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Overview.css';
+
+function Overview() {
+    return (
+        <section className="content">
+            <h2>Project Overview</h2>
+            <p>[Provide a detailed description of the project, goals, and methodology here]</p>
+        </section>
+    );
+}
+
+export default Overview;
